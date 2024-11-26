c5e1a68ae28a6187d0bea89f805d1d96b3af6770
Betül Sağlam <betul.saglam004@gmail.com>
2024-11-26 11:55:33 +0300

    backend models initialized

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,49 @@
 from django.db import models
 
-# Create your models here.
+class Albums(models.Model):
+    Album_ID = models.AutoField(primary_key = True)
+    Title = models.CharField(max_length = 100)
+    Release_date = models.DateField()
+    Artist_ID = models.ForeignKey(
+        'Artists',
+        on_delete = models.CASCADE
+    )
+
+class Playlists(models.Model):
+    Playlist_ID = models.AutoField(primary_key = True)
+    User_ID = models.ForeignKey(
+        'Users',
+        on_delete = models.CASCADE
+    )
+    Name = models.CharField(max_length = 100)
+    Created_at = models.DateField()
+    Tracks = models.JSONField(default = list)
+
+class User_Interactions:
+    Interaction_ID = models.AutoField(primary_key = True)
+    User_ID = models.ForeignKey(
+        'Users',
+        on_delete = models.CASCADE
+    )
+    Track_ID = models.ForeignKey(
+        'Tracks',
+        on_delete = models.CASCADE
+    )
+    Liked = models.BooleanField()
+    Timestamp = models.IntegerField(default = 0, max_length = 1000)
+
+class Recently_Listened:
+    Recently_Listened_ID = models.AutoField(primary_key = True)
+    User_ID = models.ForeignKey(
+        'Users',
+        on_delete = models.CASCADE
+    )
+    Track_ID = models.ForeignKey(
+        'Tracks',
+        on_delete = models.CASCADE
+    )
+    Timestamp = models.IntegerField(default = 0, max_length = 1000)
+    Play_count = models.IntegerField()
 
 class Users(models.Model):
     User_ID = models.AutoField(primary_key=True)
